46d9dbc7948db7fbfb99da77bac0e4ca5a504742
Evangelos Pittas <evangelospittas@gmail.com>
2016-02-27 11:17:08 +0200

    Added storyboard and test view in Example

---
+++ Example/EPDeckView/TestView.swift
@@ -0,0 +1,81 @@
+//
+//  TestView.swift
+//  EPDeckView
+//
+//  Created by Evangelos Pittas on 26/02/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import EPDeckView
+
+class TestView: EPCardView {
+    
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
+
+    // MARK: INITIALIZATION
+    
+    required init(frame: CGRect) {
+        super.init(frame: frame)
+        self.xibSetup()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.xibSetup()
+    }
+
+    
+    // MARK: XIB SETUP
+    func xibSetup() {
+        self.view = self.loadViewFromNib()
+        self.view.frame = bounds
+        self.view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        self.addSubview(self.view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "TestView", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width/2
+        self.profileImageView.layer.masksToBounds = true
+        
+        return view
+    }
+}
+
+
+extension UIView {
+    class func loadFromNibNamed(nibNamed: String, bundle : NSBundle? = nil) -> UIView? {
+        return UINib(
+            nibName: nibNamed,
+            bundle: bundle
+            ).instantiateWithOwner(nil, options: nil)[0] as? UIView
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
